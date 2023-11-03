c3999fc0d9891a41712c79f148293e5a4661823c
Zerone Brianstorm <ometha12@gmail.com>
2023-11-03 09:38:16 +0530

    updated data

--- read_request_from_server.py
+++ read_request_from_server.py
@@ -1,13 +1,21 @@
+from urllib import response
 import requests
 import json
 import time
+global _temp
+_temp = "null"
 def get_requests():
     display_received_content(requests.get("https://github.com/z3r0n3br4instorm/gpt_web/blob/main/read.zttf").text)
 
 def display_received_content(data):
+    global _temp
     time.sleep(5)
     try:
-        print("Received from server:", json.loads(data)["payload"]["blob"]["rawLines"][0])
+        if json.loads(data)["payload"]["blob"]["rawLines"][0] == _temp:
+            print("skip")
+        else:
+            print("Received from server:", json.loads(data)["payload"]["blob"]["rawLines"][0])
+        _temp = json.loads(data)["payload"]["blob"]["rawLines"][0]
     except:
         print("An unexpected error occured !")
 if __name__ == "__main__":
