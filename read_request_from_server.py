4043f708449adda6a74fba2ea61fddae535de4c1
Zerone Brianstorm <ometha12@gmail.com>
2023-11-03 09:29:37 +0530

    updated data

--- read_request_from_server.py
+++ read_request_from_server.py
@@ -1,10 +1,11 @@
 import requests
+import json
 
 def get_requests():
-    display_received_content(requests.get("https://raw.githubusercontent.com/z3r0n3br4instorm/gpt_web/main/read.zttf").text)
+    display_received_content(requests.get("https://github.com/z3r0n3br4instorm/gpt_web/blob/main/read.zttf").text)
 
 def display_received_content(data):
-    print("Received from server:", data)
+    print("Received from server:", json.loads(data)["payload"]["blob"]["rawLines"][0])
 
 if __name__ == "__main__":
     while True:
